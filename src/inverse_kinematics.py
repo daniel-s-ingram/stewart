03230a4b96b7c70a8c3194dbe46233f03c73b6c1
Daniel Ingram <ingramds@appstate.edu>
2018-12-03 20:49:56 -0500

    Cleaning up

--- src/inverse_kinematics.py
+++ src/inverse_kinematics.py
@@ -10,36 +10,33 @@ roll = pitch = yaw = 0.0
 height = 2.0 #Height of platform above base when legs are completely unextended
 
 b = np.array([[-0.101, 0.8, 0.25],
-			  [0.101, 0.8, 0.25],
-			  [0.743, -0.313, 0.25],
-			  [0.642, -0.487, 0.25],
-			  [-0.643, -0.486, 0.25],
-			  [-0.744, -0.311, 0.25]])
+              [0.101, 0.8, 0.25],
+              [0.743, -0.313, 0.25],
+              [0.642, -0.487, 0.25],
+              [-0.643, -0.486, 0.25],
+              [-0.744, -0.311, 0.25]])
 
 p = np.array([[-0.642, 0.487, -0.05],
-			  [0.642, 0.487, -0.05],
-			  [0.743, 0.313, -0.05],
-			  [0.101, -0.8, -0.05],
-			  [-0.101, -0.8, -0.05],
-			  [-0.743, 0.313, -0.05]])
+              [0.642, 0.487, -0.05],
+              [0.743, 0.313, -0.05],
+              [0.101, -0.8, -0.05],
+              [-0.101, -0.8, -0.05],
+              [-0.743, 0.313, -0.05]])
 
 def twist_callback(twist_msg):
-	global x, y, z, roll, pitch, yaw
-	x = twist_msg.linear.x
-	y = twist_msg.linear.y
-	z = twist_msg.linear.z
+    global x, y, z, roll, pitch, yaw
+    x = twist_msg.linear.x
+    y = twist_msg.linear.y
+    z = twist_msg.linear.z
 
-	roll = twist_msg.angular.x
-	pitch = twist_msg.angular.y
-	yaw = twist_msg.angular.z
+    roll = twist_msg.angular.x
+    pitch = twist_msg.angular.y
+    yaw = twist_msg.angular.z
 
-#roll -> rho
-#pitch -> theta
-#yaw -> psi
 def rotation_matrix(rho, theta, psi):
-	return np.array([[cos(psi)*cos(theta), -sin(psi)*cos(rho)+cos(psi)*sin(theta)*sin(rho), sin(psi)*sin(rho)+cos(psi)*sin(theta)*cos(rho)],
-					 [sin(psi)*cos(theta), cos(psi)*cos(rho)+sin(psi)*sin(theta)*sin(rho), -cos(psi)*sin(rho)+sin(psi)*sin(theta)*cos(rho)],
-					 [-sin(theta), cos(theta)*sin(rho), cos(theta)*cos(psi)]])
+    return np.array([[cos(psi)*cos(theta), -sin(psi)*cos(rho)+cos(psi)*sin(theta)*sin(rho), sin(psi)*sin(rho)+cos(psi)*sin(theta)*cos(rho)],
+                     [sin(psi)*cos(theta), cos(psi)*cos(rho)+sin(psi)*sin(theta)*sin(rho), -cos(psi)*sin(rho)+sin(psi)*sin(theta)*cos(rho)],
+                     [-sin(theta), cos(theta)*sin(rho), cos(theta)*cos(psi)]])
 
 rospy.init_node('inverse_kinematics', anonymous=True)
 
@@ -47,14 +44,13 @@ twist_sub = rospy.Subscriber('/stewart/platform_twist', Twist, twist_callback)
 piston_pub = rospy.Publisher('/stewart/piston_cmd', Float32MultiArray, queue_size=20)
 
 piston_lengths = Float32MultiArray()
-piston_lengths.data = [0,0,0,0,0,0]
+piston_lengths.data = [0, 0, 0, 0, 0, 0]
 n_pistons = len(piston_lengths.data)
-
 length_components = [0, 0, 0]
 
 while not rospy.is_shutdown():
-	for i in xrange(n_pistons):
-		length_components = np.array([x, y, z+height]) + np.dot(rotation_matrix(roll, pitch, yaw), p[i]) - b[i]
-		piston_lengths.data[i] = np.sqrt(length_components[0]**2 + length_components[1]**2 + length_components[2]**2)-1.93
+    for i in xrange(n_pistons):
+        length_components = np.array([x, y, z+height]) + np.dot(rotation_matrix(roll, pitch, yaw), p[i]) - b[i]
+        piston_lengths.data[i] = np.sqrt(length_components[0]**2 + length_components[1]**2 + length_components[2]**2)-1.93
 
-	piston_pub.publish(piston_lengths)
+    piston_pub.publish(piston_lengths)
--- src/ps4_controller.py
+++ src/ps4_controller.py
@@ -1,52 +1,28 @@
 #!/usr/bin/env python
+from math import pi
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
-x = y = z = 0.0
-roll = pitch = yaw = 0.0
-
-def joy_callback(msg):
-	global x, y, z, roll, pitch, yaw
-
-	roll = -msg.axes[4]
-	pitch = -msg.axes[3]
-	if msg.buttons[5] and not msg.buttons[4]:
-		yaw = yaw + 0.01
-	elif msg.buttons[4] and not msg.buttons[5]:
-		yaw = yaw - 0.01	
-
-	x = -msg.axes[0]
-	y = msg.axes[1]
-	if msg.buttons[7] and not msg.buttons[6]:
-		z = z + 0.01
-	elif msg.buttons[6] and not msg.buttons[7]:
-		z = z - 0.01
-
-	if z > 1:
-		z = 1
-	elif z < 0:
-		z = 0
-
-	if yaw > 1.571:
-		yaw = 1.571
-	elif yaw < -1.571:
-		yaw = -1.571
-
-rospy.init_node('ps4_controller', anonymous=True)
-
-joy_sub = rospy.Subscriber('/joy', Joy, joy_callback)
-twist_pub = rospy.Publisher('stewart/platform_twist', Twist, queue_size=100)
-
 platform_twist = Twist()
 
-while not rospy.is_shutdown():
-	platform_twist.linear.x = x
-	platform_twist.linear.y = y
-	platform_twist.linear.z = z
-
-	platform_twist.angular.x = roll
-	platform_twist.angular.y = pitch
-	platform_twist.angular.z = yaw
+def joy_callback(msg):
+	platform_twist.linear.x = -msg.axes[0]
+	platform_twist.linear.y = msg.axes[1]
+	platform_twist.angular.x = -msg.axes[4]
+	platform_twist.angular.y = -msg.axes[3]
+	if msg.buttons[7] and platform_twist.linear.z < 1:
+		platform_twist.linear.z += 0.01
+	elif msg.buttons[6] and platform_twist.linear.z > 0:
+		platform_twist.linear.z -= 0.01
+	if msg.buttons[5] and platform_twist.angular.z < pi/2:
+		platform_twist.angular.z += 0.01
+	elif msg.buttons[4] and platform_twist.angular.z > -pi/2:
+		platform_twist.angular.z -= 0.01	
 
 	twist_pub.publish(platform_twist)
+
+rospy.init_node('ps4_controller', anonymous=True)
+joy_sub = rospy.Subscriber('/joy', Joy, joy_callback)
+twist_pub = rospy.Publisher('stewart/platform_twist', Twist, queue_size=10)
+rospy.spin()
