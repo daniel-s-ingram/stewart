eb4cf2f4a9c8f349565a0d24e373e9236d890624
Daniel Ingram <ingramds@appstate.edu>
2018-12-27 17:54:24 -0500

    Update plugin

--- plugin/joint_controller.cc
+++ plugin/joint_controller.cc
@@ -40,46 +40,45 @@ namespace gazebo
         std::cout << "\nThe JointController plugin is attach to model[" << _model->GetName() << "]\n";
       }
 
-      this->model = _model;
-      this->pid = common::PID(1000.0, 0.1, 100.0);
+      model = _model;
+      pid = common::PID(1000.0, 0.1, 100.0);
       for(i = 0; i < 6; i++)
       {
-        this->piston[i] = _model->GetJoints()[i];
-        this->model->GetJointController()->SetPositionPID(this->piston[i]->GetScopedName(), this->pid);
+        piston[i] = _model->GetJoints()[i];
+        this->model->GetJointController()->SetPositionPID(piston[i]->GetScopedName(), pid);
       }
 
       if (!ros::isInitialized())
       {
         int argc = 0;
         char **argv = NULL;
-        ros::init(argc, argv, "gazebo_client",
-            ros::init_options::NoSigintHandler);
+        ros::init(argc, argv, "gazebo_client", ros::init_options::NoSigintHandler);
       }
 
-      this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
-      ros::SubscribeOptions so =ros::SubscribeOptions::create<std_msgs::Float32MultiArray>("/" + this->model->GetName() + "/piston_cmd", 
-                                                                                          100, 
-                                                                                          boost::bind(&JointController::SetPosition, this, _1),
-                                                                                          ros::VoidPtr(), &this->rosQueue);
+      rosNode.reset(new ros::NodeHandle("gazebo_client"));
+      ros::SubscribeOptions so =ros::SubscribeOptions::create<std_msgs::Float32MultiArray>("/" + model->GetName() + "/piston_cmd", 
+                                                                                           100, 
+                                                                                           boost::bind(&JointController::SetPosition, this, _1),
+                                                                                           ros::VoidPtr(), &this->rosQueue);
       
-      this->rosSub = this->rosNode->subscribe(so);
-      this->rosQueueThread = std::thread(std::bind(&JointController::QueueThread, this));
+      rosSub = rosNode->subscribe(so);
+      rosQueueThread = std::thread(std::bind(&JointController::QueueThread, this));
     }
 
     public: void SetPosition(const std_msgs::Float32MultiArray::ConstPtr& msg)
     {
       for(int i = 0; i < 6; i++)
       {
-        this->model->GetJointController()->SetPositionTarget(this->piston[i]->GetScopedName(), msg->data[i]);
+        model->GetJointController()->SetPositionTarget(piston[i]->GetScopedName(), msg->data[i]);
       }
     }
 
     private: void QueueThread()
     {
       static const double timeout = 0.01;
-      while (this->rosNode->ok())
+      while (rosNode->ok())
       {
-        this->rosQueue.callAvailable(ros::WallDuration(timeout));
+        rosQueue.callAvailable(ros::WallDuration(timeout));
       }
     }
 
